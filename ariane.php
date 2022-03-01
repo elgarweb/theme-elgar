523400021b51bcb9cdad5117ef6cedc0953f9c7a
Simon Vandaele <simonvdfr@users.noreply.github.com>
2022-03-01 17:44:34 +0100

    Update ariane.php

--- ariane.php
+++ ariane.php
@@ -3,18 +3,19 @@ if(!$GLOBALS['domain']) exit;
 
 // @todo voir pour mettre le fil d'ariane dans une ol/li => problème avec système de tag+edition
 // Peut-être injecter accueil dans la liste de tag et le supp au moment du save... séparateur "/"
+// itemprop="breadcrumb" ?
 ?>
 <div class="ariane mod ptm pbm">
 
 	<nav role="navigation" aria-label="<?php _e("Breadcrumb")?>" class="fl">
 		<a href="/"><?php _e("Home")?></a>
-		/
+		>
 		<?php
 		// @todo
 		// Si page "navigation" on récupère le nom du tag/filtre
 
 		// Sinon on propose de saisir le tag correspondant
-		tag('navigation', array('tag' => 'span', 'separator' => ' / '));//'itemprop' => 'breadcrumb',
+		tag('navigation', array('tag' => 'span', 'separator' => ' > '));
 		?>
 	</nav>
 
