07289f3935d6389634c53dae3a09f9ccc3bf679d
Simon VD <simonvdfr@users.noreply.github.com>
2024-12-25 20:41:10 +0100

    ajout de page sommaire avec grille de contenu

--- tpl/page-sommaire.php
+++ tpl/page-sommaire.php
@@ -20,11 +20,60 @@ if(@$content['intranet'] != 'true' or (@$content['intranet'] == 'true' and (isse
 				</nav>
 			</div>
 
-			<article class="mw960p">
 
-				<?php txt('texte', array('lazy' => true)); ?>
+			<?php if($res['tpl'] == 'page-sommaire-grille'){?>
+				
+				
+				<article class="mw960p minw320p">
 
-			</article>
+					<?php txt('description'); ?>
+
+					<div>
+					<ul id="groupe" class="module unstyled pan">
+					<?php
+					$groupe = module("groupe");
+					//highlight_string(print_r($groupe, true));
+					foreach($groupe as $i => $val)
+					{ ?>
+						<li>
+
+							<?php
+							h2('groupe-titre-'.$i);
+							txt('groupe-texte-'.$i);
+							?>
+
+							<ul id="grille-<?=$i?>" class="module unstyled pan end grid-4 space-xl">
+							<?php 
+							$grille = module('grille-'.$i);
+							//highlight_string(print_r($grille, true));
+							foreach($grille as $k => $val) { ?>
+								<li>
+									<?php media('grille-'.$i.'-visuel-'.$k, array('size' => '150x150', 'lazy' => true));?>
+									<?php txt('grille-'.$i.'-texte-'.$k, array("class" => ""));?>
+								</li>
+							<?php }	?>
+							</ul>		
+
+						</li>
+					<?php } ?>
+					</ul>
+					</div>
+
+				</article>
+
+
+			<?php }else{?>
+
+
+				<article class="mw960p">
+
+					<?php txt('texte', array('lazy' => true)); ?>
+
+				</article>
+
+
+			<?php }?>
+
 
 		</div>
 
